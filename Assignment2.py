486ff4432f3d358256d894d12ac47dfa48d21c90
LovelyAmirah14 <amirah.banton001@albright.edu>
2025-09-29 08:49:04 -0400

    did some work

---
+++ Assignment2.py
@@ -0,0 +1,28 @@
+# Assignment 2 Banton, Amirah
+seperate = "--------------------------------------------------"
+
+# 2-1 Simple Message
+message = "I'm steve"
+print(message)
+
+print(seperate)
+
+# 2-2 Simple Messages
+words = "You're amazing and cool,"
+print(words)
+words = "but you have some flaws you could fix." # setting the variable to something else
+print(words) # this is printing the new changes variable.
+
+print(seperate)
+
+# Book notes 
+# When wanting to call uppercase or lowercase you can call the variable (variable).upper or (variable).lower
+# example 
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+# You can combine first name and last name together
+
+print(seperate)
+
+# 2-3 
---
+++ week6.py
@@ -0,0 +1,5 @@
+names = ["Brian", "Sarah", "Peter", "Mary"]
+salaries =[25,20,40,50]
+ages = [45,20,50,30]
+
+employees 
