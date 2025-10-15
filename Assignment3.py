9beb1fb2ac32dcf7cc70b062dfb59b13e015829e
LovelyAmirah14 <Amirah.Banton001@albright.edu>
2025-10-15 10:52:16 -0400

    did assignment 3 completed

--- Assignment3.py
+++ Assignment3.py
@@ -49,3 +49,57 @@ print("THE OFFICIAL GUEST LIST: " + guest_list[0] + ", " + guest_list[1] + ", " 
 
 # 3-7 Shrinking Guest List
 
+print("OH NO! The new table won't arrive on time and I can only invite two people to the party now.")
+removed_guest = guest_list.pop() # removing the last person on the list
+print("Sorry " + removed_guest + ", I can't invite you to the party.")
+removed_guest = guest_list.pop() # removing the last person on the list
+print("Sorry " + removed_guest + ", I can't invite you to the party.")
+removed_guest = guest_list.pop() # removing the last person on the list
+print("Sorry " + removed_guest + ", I can't invite you to the party.")
+removed_guest = guest_list.pop() # removing the last person on the list
+print("Sorry " + removed_guest + ", I can't invite you to the party.")
+print("THE OFFICIAL GUEST LIST: " + guest_list[0] + ", "
+        + guest_list[1]) # printing the new guest list
+del guest_list[1] # removing the last two people on the list
+del guest_list[0]
+print(guest_list) # printing the empty list
+
+print(seperate)
+
+# 3-8 Seeing the World
+places = ["Japan", "Italy", "Greece", "France", "Spain"]
+print(places) # original order
+print(sorted(places)) # alphabetical order
+print(places) # original order
+print(sorted(places, reverse=True)) # reverse alphabetical order
+print(places) # original order
+places.reverse() # reversing the list
+print(places) # printing the reversed list
+places.reverse() # reversing the list back to original
+print(places) # printing the original list
+places.sort() # sorting the list in alphabetical order
+print(places) # printing the sorted list
+places.sort(reverse=True) # sorting the list in reverse alphabetical order 
+print(places) # printing the reverse sorted list
+
+print(seperate)
+
+# 3-9 Dinner Guests
+print("I am inviting " + str(len(guest_list)) + " people to my party.") # printing the number of people invited to the party
+# there is no one invited to the party so it prints 0
+print("I am inviting " + str(len(places)) + " places I want to visit.") # printing the number of places I want to visit
+# there is 5 places I want to visit so it prints 5
+
+print(seperate)
+
+# 3-10 Every Function
+# I have already done every function in the previous exercises.
+# I have used len(), append(), insert(), pop(), del(), sort(), sorted(), reverse(), and print() functions in the previous exercises.
+# I have not used remove() function yet.
+# I will use remove() function to remove a specific item from a list.
+places.remove("Greece") # removing Greece from the list of places
+print(places) # printing the list after removing Greece
+# now the list of places is ['Spain', 'Italy', 'France', 'Japan'] after removing Greece
+
+print(seperate)
+# End of Assignment 3
