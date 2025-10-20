8ac07aca6604a01b045ee60ee96d182400494db9
LovelyAmirah14 <amirah.banton001@albright.edu>
2025-10-20 09:30:15 -0400

    fixed errors

---
+++ Classwork.py
@@ -0,0 +1,23 @@
+# Work done in class can be seen here
+# I will use seperate and put dates so that it's secluded
+
+seperate = "-----------------"
+
+# Date Monday 10/20/2025 
+
+def greet(name): 
+    print(f"Hi, {name}")
+
+people = ["Joe", "Mary", "Sal"]
+
+for person in people:
+    greet(person)
+
+seperate()
+
+# password check
+# at least 15 characters
+# should have a number
+# should have a capital letters
+# should have a lower case letter
+def password_check(password):
